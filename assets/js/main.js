99ef4f4f27c9b2409bb1d7de95c82eaae2c634f3
aytacallahverdiyeva <aytac.allahverdiyeva@gmail.com>
2023-04-03 05:44:30 +0400

    committing

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//!method one(maybe not correct)
+// let a = 5;
+// let b = 6;
+// let result = `  ${6}  ${5}`;
+// console.log(result);
+
+//!method two
+let a = 5;
+let b = 6;
+
+if (a < b) {
+	let a = 6;
+	console.log(a);
+};
+console.log(a);
